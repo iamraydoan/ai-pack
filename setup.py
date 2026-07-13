82ff8a612b269dd8a577d3dc73f8553f9aa9aa32
Ray Doan <raydoan.contact@gmail.com>
2026-07-13 10:32:51 +0700

    chore: bump version to 0.1.1 and include long description in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+# Read the contents of README.md to use as the long description on PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="ai-pack-cli",
-    version="0.1.0",
+    version="0.1.1",
     py_modules=["ai_pack"],
     install_requires=[
         "pyperclip>=1.8.2",
@@ -15,4 +19,12 @@ setup(
             "aip=ai_pack:main",
         ],
     },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamraydoan/ai-pack",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
